3fde43ca3ba6327c4cf966ce19d5dff2bb0a7de5
Angel Gonzalez <angel.gonzalezh08@gmail.com>
2024-05-15 02:44:24 -0600

    portfolio concept

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },
-  {
-    path: 'index',
-    loadChildren: () => import('./index/index.module').then( m => m.IndexPageModule)
   },
 ];
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  projects = [
+    {
+      title: 'Project 1',
+      imageUrl: 'https://placehold.co/400x230',
+      description: 'Quae facere obcaecati dignissimos a unde. Accusamus nobis vitae rerum id impedit.',
+      repoUrl: 'https://github.com/user/project1'
+    },
+    {
+      title: 'Project 2',
+      imageUrl: 'https://placehold.co/400x230',
+      description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      repoUrl: 'https://github.com/user/project2'
+    },
+    {
+      title: 'Project 3',
+      imageUrl: 'https://placehold.co/400x230',
+      description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      repoUrl: 'https://github.com/user/project3'
+    },
+  ];
+
+  constructor() { }
 
 }
